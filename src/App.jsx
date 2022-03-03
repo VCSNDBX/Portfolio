7b8153b96614df4006340a449671d6f2eb7f85e2
Victor <vbcatapat@gmail.com>
2022-03-04 02:03:28 +0400

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+<<<<<<< HEAD
 
 const app = () => {
   return (
     <div>Hello World!</div>
+=======
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const app = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+>>>>>>> components
   )
 }
 
